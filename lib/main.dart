8c8228af16522430668751863c4d36770d55e22b
Deepak Patel <deepak@divyaltech.com>
2022-12-28 20:32:00 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    int day = 30;
+    return MaterialApp(
+        home:
+            Material(child: Center(child: Text("welcome to $day of flutter"))));
+  }
+}
